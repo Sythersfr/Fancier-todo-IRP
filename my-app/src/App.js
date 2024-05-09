6f2457003971608ae45a4587298d4c386714e32f
Sythersfr <npotoplyak@gmail.com>
2024-05-09 13:37:08 -0500

    week 4 code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import useSound from 'use-sound';
 import './App.css';
+import sound1 from './sounds/tenge-tenge.mp3'
+import sound2 from './sounds/sus.mp3'
+import sound3 from './sounds/sad.mp3'
 
 function App() {
   const [slam, setSlam] = useState(false);
@@ -9,31 +11,42 @@ function App() {
 
   //adding todos to list
 
+  function play1(){
+    new Audio(sound1).play()
+  }
+  function play2(){
+    new Audio(sound2).play()
+  }
+  function play3(){
+    new Audio(sound3).play()
+  }
+
+  const triggerRainbowBackground = () => {
+    const appElement = document.querySelector('.App');
+    appElement.classList.add('rainbow-background');
+    setTimeout(() => {
+      appElement.classList.remove('rainbow-background');
+    }, 12000); 
+  };
+
   const addTodo = (event) => {
     event.preventDefault();
     setTodos([...todos, {id: Date.now(), text: input, completed: false, slam: true}]);
     setInput('');
+    play2()
   }
 
   //removing todos from lsit
   const removeTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
+    play3()
   }
   //displayig todos as finished
   const completeTodo = (id) => {
     setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
-  
+    play1()
+    triggerRainbowBackground();
   }
-
-  const [playSound] = useSound('/sounds/sus.mp3');
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setTodos(todos.map(todo => ({...todo, slam: false})));
-    }, 1000);
-  
-    return () => clearTimeout(timer); 
-  }, [todos]);
 
   return (
     <div className="App">
@@ -50,7 +63,7 @@ function App() {
           <li key={todo.id} className={todo.slam ? 'slam' : ''} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
             <div>{todo.text}</div>
             <div>
-              <button id = 'c_r_btn' onClick={playSound}>Complete</button>
+              <button id = 'c_r_btn' onClick={() => completeTodo(todo.id)}>Complete</button>
               <button id = 'c_r_btn' onClick={() => removeTodo(todo.id)}>Remove</button>
             </div>
           </li>
